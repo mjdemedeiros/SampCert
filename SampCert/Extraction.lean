0ad0757015ae5ecd92f5d6b9bdea49499b0c0a58
de Medeiros <markusjd@amazon.com>
2024-05-31 14:00:24 -0400

    (wip) SampCert dependecy gone

--- SampCert/Extraction.lean
+++ SampCert/Extraction.lean
@@ -24,31 +24,31 @@ open SLang
 
 -- PROBLEM 1: How to get the extractor to unfold the definitions we want it to
 
-#eval Lean.Expr.const ``SLang.probUntil []
+-- #eval Lean.Expr.const ``SLang.probUntil []
 -- Reduce doesn't work (I don't expect it to) so I need some other way to rewrite via def. eq's.
 -- #reduce SLang.probUntil
 -- #eval Lean.Meta.reduce (Lean.Expr.const ``SLang.probUntil [])
 
 -- This helper function will perform elaboration of the Lean-level syntax and then apply whnf.
-open Lean.Elab.Term in
-def whnf' (e : TermElabM Syntax) : TermElabM Format := do
-  let e ← elabTermAndSynthesize (← e) none
-  let w <- withTransparency .reducible $ whnf e
-  ppExpr w
-
-attribute [reducible] List.cons
-#eval whnf' `(List.cons 1 [])
-attribute [irreducible] List.cons
-#eval whnf' `(List.cons 1 [])
-
-def body' : SLang Nat := pure 4
-def cond' : Nat -> Bool := fun _ => true
--- #check (probUntil body' cond')
-#eval whnf' `(probUntil body' cond')
-attribute [reducible] probUntil
-#eval whnf' `(probUntil body' cond')                      -- Unfolded (up to probWhile)
-#eval whnf' `(probWhile cond' (fun _ => body'))           -- Not unfolded
-#eval whnf' `(probUntil body' cond' 5)                    -- Unfolded (up to body')
+-- open Lean.Elab.Term in
+-- def whnf' (e : TermElabM Syntax) : TermElabM Format := do
+--   let e ← elabTermAndSynthesize (← e) none
+--   let w <- withTransparency .reducible $ whnf e
+--   ppExpr w
+--
+-- attribute [reducible] List.cons
+-- #eval whnf' `(List.cons 1 [])
+-- attribute [irreducible] List.cons
+-- #eval whnf' `(List.cons 1 [])
+--
+-- def body' : SLang Nat := pure 4
+-- def cond' : Nat -> Bool := fun _ => true
+-- -- #check (probUntil body' cond')
+-- #eval whnf' `(probUntil body' cond')
+-- attribute [reducible] probUntil
+-- #eval whnf' `(probUntil body' cond')                      -- Unfolded (up to probWhile)
+-- #eval whnf' `(probWhile cond' (fun _ => body'))           -- Not unfolded
+-- #eval whnf' `(probUntil body' cond' 5)                    -- Unfolded (up to body')
 
 -- LGTM! So somehow we need to figure out how to manage the attributes at extraction time
 -- since we cannot control the attributes that the users put on their DSL terms.
@@ -65,37 +65,37 @@ attribute [reducible] probUntil
 section test_reducibility
 
 
-def PNat_1_expr : Expr :=
- Lean.Expr.app
-   (Lean.Expr.app
-     (Lean.Expr.app
-       (Lean.Expr.app (Lean.Expr.const `Subtype.mk [Lean.Level.succ (Lean.Level.zero)]) (Lean.Expr.const `Nat []))
-       (Lean.Expr.lam
-         `n
-         (Lean.Expr.const `Nat [])
-         (Lean.Expr.app
-           (Lean.Expr.app
-             (Lean.Expr.app
-               (Lean.Expr.app (Lean.Expr.const `LT.lt [Lean.Level.zero]) (Lean.Expr.const `Nat []))
-               (Lean.Expr.const `instLTNat []))
-             (Lean.Expr.app
-               (Lean.Expr.app
-                 (Lean.Expr.app (Lean.Expr.const `OfNat.ofNat [Lean.Level.zero]) (Lean.Expr.const `Nat []))
-                 (Lean.Expr.lit (Lean.Literal.natVal 0)))
-               (Lean.Expr.app (Lean.Expr.const `instOfNatNat []) (Lean.Expr.lit (Lean.Literal.natVal 0)))))
-           (Lean.Expr.bvar 0))
-         (Lean.BinderInfo.default)))
-     (Lean.Expr.lit (Lean.Literal.natVal 1)))
-   (Lean.Expr.app
-     (Lean.Expr.app (Lean.Expr.const `instOfNatPNatOfNeZeroNat.proof_1 []) (Lean.Expr.lit (Lean.Literal.natVal 1)))
-     (Lean.Expr.const `testPNat.proof_1 []))
-
-def US_test : MetaM Expr := do
-  let (e : Expr) <- mkAppM ``UniformSample #[PNat_1_expr]
-  let (e1) <- withTransparency .reducible $ whnf (Lean.mkApp (Lean.Expr.const ``UniformSample []) PNat_1_expr)
-  return e1
-
-#eval US_test
+-- def PNat_1_expr : Expr :=
+--  Lean.Expr.app
+--    (Lean.Expr.app
+--      (Lean.Expr.app
+--        (Lean.Expr.app (Lean.Expr.const `Subtype.mk [Lean.Level.succ (Lean.Level.zero)]) (Lean.Expr.const `Nat []))
+--        (Lean.Expr.lam
+--          `n
+--          (Lean.Expr.const `Nat [])
+--          (Lean.Expr.app
+--            (Lean.Expr.app
+--              (Lean.Expr.app
+--                (Lean.Expr.app (Lean.Expr.const `LT.lt [Lean.Level.zero]) (Lean.Expr.const `Nat []))
+--                (Lean.Expr.const `instLTNat []))
+--              (Lean.Expr.app
+--                (Lean.Expr.app
+--                  (Lean.Expr.app (Lean.Expr.const `OfNat.ofNat [Lean.Level.zero]) (Lean.Expr.const `Nat []))
+--                  (Lean.Expr.lit (Lean.Literal.natVal 0)))
+--                (Lean.Expr.app (Lean.Expr.const `instOfNatNat []) (Lean.Expr.lit (Lean.Literal.natVal 0)))))
+--            (Lean.Expr.bvar 0))
+--          (Lean.BinderInfo.default)))
+--      (Lean.Expr.lit (Lean.Literal.natVal 1)))
+--    (Lean.Expr.app
+--      (Lean.Expr.app (Lean.Expr.const `instOfNatPNatOfNeZeroNat.proof_1 []) (Lean.Expr.lit (Lean.Literal.natVal 1)))
+--      (Lean.Expr.const `testPNat.proof_1 []))
+--
+-- def US_test : MetaM Expr := do
+--   let (e : Expr) <- mkAppM ``UniformSample #[PNat_1_expr]
+--   let (e1) <- withTransparency .reducible $ whnf (Lean.mkApp (Lean.Expr.const ``UniformSample []) PNat_1_expr)
+--   return e1
+--
+-- #eval US_test
 
 -- OK so it seems like the test is just not a good test-- doing it this way adds in a bunch of explicit monad
 -- instance and universe level junk. I'll just test inside the extractor instead.
@@ -119,11 +119,11 @@ end test_reducibility
 -- PROBLEM 2: How to get the frontend to infer the Capsid typeclass itself?
 
 -- infers universe cosntants, maybe also typeclasses??
-#check Lean.Meta.mkAppM
+-- #check Lean.Meta.mkAppM
 
 
 -- The WF normalization thing: What is the notion of a monadic (extractable) program?
-#check Lean.Meta.forallTelescope
+-- #check Lean.Meta.forallTelescope
 
 
 /-! Extraction using Capsid
@@ -141,6 +141,8 @@ Additionally, the following names are protected:
 
 instance SLang_Capsid : Capsid SLang where
   capsWhile := probWhile
+  capsUntil := probUntil
+
 
 
 def testSLang : SLang Nat := (return 5) >>= (fun x => x)
@@ -164,24 +166,38 @@ def encapsulate {T U : Type*} [HC : Capsid M] (f : T -> M U) : (Capsid M × (T -
 
 -- MARKUSDE: Push encapsulate into the attribute?
 
-attribute [reducible] probUntil
-
-def testCapsid := (SLang_Capsid, UniformSample)
-attribute [export_dafny] testCapsid
+-- attribute [reducible] probUntil
 
 
--- attribute [export_dafny] UniformSample
--- attribute [export_dafny] BernoulliSample
--- attribute [export_dafny] BernoulliExpNegSampleUnitLoop
--- attribute [export_dafny] BernoulliExpNegSampleUnitAux
--- attribute [export_dafny] BernoulliExpNegSampleUnit
--- attribute [export_dafny] BernoulliExpNegSampleGenLoop
--- attribute [export_dafny] BernoulliExpNegSample
--- attribute [export_dafny] DiscreteLaplaceSampleLoopIn1Aux
--- attribute [export_dafny] DiscreteLaplaceSampleLoopIn1
--- attribute [export_dafny] DiscreteLaplaceSampleLoopIn2Aux
--- attribute [export_dafny] DiscreteLaplaceSampleLoopIn2
--- attribute [export_dafny] DiscreteLaplaceSampleLoop
--- attribute [export_dafny] DiscreteLaplaceSample
--- attribute [export_dafny] DiscreteGaussianSampleLoop
--- attribute [export_dafny] DiscreteGaussianSample
+def tc_UniformSample := (SLang_Capsid, UniformSample)
+def tc_BernoulliSample := (SLang_Capsid, BernoulliSample)
+def tc_BernoulliExpNegSampleUnitLoop := (SLang_Capsid, BernoulliExpNegSampleUnitLoop)
+def tc_BernoulliExpNegSampleUnitAux := (SLang_Capsid, BernoulliExpNegSampleUnitAux )
+def tc_BernoulliExpNegSampleUnit := (SLang_Capsid, BernoulliExpNegSampleUnit )
+def tc_BernoulliExpNegSampleGenLoop := (SLang_Capsid, BernoulliExpNegSampleGenLoop )
+def tc_BernoulliExpNegSample := (SLang_Capsid, BernoulliExpNegSample )
+def tc_DiscreteLaplaceSampleLoopIn1Aux := (SLang_Capsid, DiscreteLaplaceSampleLoopIn1Aux )
+def tc_DiscreteLaplaceSampleLoopIn1 := (SLang_Capsid, DiscreteLaplaceSampleLoopIn1)
+def tc_DiscreteLaplaceSampleLoopIn2Aux := (SLang_Capsid, DiscreteLaplaceSampleLoopIn2Aux )
+def tc_DiscreteLaplaceSampleLoopIn2 := (SLang_Capsid, DiscreteLaplaceSampleLoopIn2)
+def tc_DiscreteLaplaceSampleLoop := (SLang_Capsid, DiscreteLaplaceSampleLoop )
+def tc_DiscreteLaplaceSample := (SLang_Capsid, DiscreteLaplaceSample )
+def tc_DiscreteGaussianSampleLoop := (SLang_Capsid, DiscreteGaussianSampleLoop )
+def tc_DiscreteGaussianSample := (SLang_Capsid, DiscreteGaussianSample)
+
+
+attribute [export_dafny] tc_UniformSample
+attribute [export_dafny] tc_BernoulliSample
+attribute [export_dafny] tc_BernoulliExpNegSampleUnitLoop
+attribute [export_dafny] tc_BernoulliExpNegSampleUnitAux
+attribute [export_dafny] tc_BernoulliExpNegSampleUnit
+-- attribute [export_dafny] tc_BernoulliExpNegSampleGenLoop
+attribute [export_dafny] tc_BernoulliExpNegSample
+attribute [export_dafny] tc_DiscreteLaplaceSampleLoopIn1Aux
+attribute [export_dafny] tc_DiscreteLaplaceSampleLoopIn1
+attribute [export_dafny] tc_DiscreteLaplaceSampleLoopIn2Aux
+attribute [export_dafny] tc_DiscreteLaplaceSampleLoopIn2
+-- attribute [export_dafny] tc_DiscreteLaplaceSampleLoop
+attribute [export_dafny] tc_DiscreteLaplaceSample
+attribute [export_dafny] tc_DiscreteGaussianSampleLoop
+attribute [export_dafny] tc_DiscreteGaussianSample
--- SampCert/Extractor/Abstract.lean
+++ SampCert/Extractor/Abstract.lean
@@ -12,44 +12,50 @@ Authors: Jean-Baptiste Tristan
 class Capsid (CapsM : Type u -> Type v) extends Monad CapsM where
   capsWhile : (cond : T → Bool) → (body : T → CapsM T) → T → CapsM T
 
+  -- This shoud be removed
+  capsUntil : (body : CapsM T) -> (cond : T → Bool) -> CapsM T
 
-section capsid_wf
-  variable {CapsM : Type u -> Type v} [C : Capsid CapsM]
-  variable {T : Type u}
-  variable (cond : T -> Bool) (body : T -> CapsM T)
 
-  open Capsid
 
-  def capsIter (fuel : Nat) : T -> (OptionT CapsM) T
-    := fun t =>
-       match fuel with
-       | Nat.zero => failure
-       | Nat.succ fuel' =>
-          if cond t
-            then (body t) >>= (capsIter fuel')
-            else pure t
 
-  def capsIterPartial (fuel : Nat) : T -> CapsM T
-    := fun t =>
-       match fuel with
-       | Nat.zero => return t
-       | Nat.succ fuel' =>
-          if cond t
-            then (body t) >>= (capsIterPartial fuel')
-            else pure t
 
-  -- Partial correctness specification for While
-  -- Since we're going to translate it into a while loop, we want specify that the shallow embedding to behave like a
-  -- the limit of loop iterations.
-  def CapsWhileSpec : Prop
-    := ∀ t0, capsWhile cond body t0 = sorry
---         ∀ t0 : T, ∃ i : Nat, ((∀ j : Nat, j < i -> iter body cond j t0 = failure) ∧ ¬ (iter body cond i t0 = failure))
---     -> iter body cond i t0 = loop T body cond t0
---     Err... This is not right. Maybe we require that the well-formed Capsid instances are topological types?
---     Do we want to say that "terminating executions of loops equal a finite unrolling" or "loops are the
---     limit of finite unrollings"?
-
-end capsid_wf
-
-class CapsidWF (CapsM : Type u -> Type v) extends Capsid CapsM where
-  capsWhileWF : ∀ (cond : T -> Bool), ∀ (body : T -> CapsM T), CapsWhileSpec cond body
+-- section capsid_wf
+--   variable {CapsM : Type u -> Type v} [C : Capsid CapsM]
+--   variable {T : Type u}
+--   variable (cond : T -> Bool) (body : T -> CapsM T)
+--
+--   open Capsid
+--
+--   def capsIter (fuel : Nat) : T -> (OptionT CapsM) T
+--     := fun t =>
+--        match fuel with
+--        | Nat.zero => failure
+--        | Nat.succ fuel' =>
+--           if cond t
+--             then (body t) >>= (capsIter fuel')
+--             else pure t
+--
+--   def capsIterPartial (fuel : Nat) : T -> CapsM T
+--     := fun t =>
+--        match fuel with
+--        | Nat.zero => return t
+--        | Nat.succ fuel' =>
+--           if cond t
+--             then (body t) >>= (capsIterPartial fuel')
+--             else pure t
+--
+--   -- Partial correctness specification for While
+--   -- Since we're going to translate it into a while loop, we want specify that the shallow embedding to behave like a
+--   -- the limit of loop iterations.
+--   def CapsWhileSpec : Prop
+--     := ∀ t0, capsWhile cond body t0 = sorry
+-- --         ∀ t0 : T, ∃ i : Nat, ((∀ j : Nat, j < i -> iter body cond j t0 = failure) ∧ ¬ (iter body cond i t0 = failure))
+-- --     -> iter body cond i t0 = loop T body cond t0
+-- --     Err... This is not right. Maybe we require that the well-formed Capsid instances are topological types?
+-- --     Do we want to say that "terminating executions of loops equal a finite unrolling" or "loops are the
+-- --     limit of finite unrollings"?
+--
+-- end capsid_wf
+--
+-- class CapsidWF (CapsM : Type u -> Type v) extends Capsid CapsM where
+--   capsWhileWF : ∀ (cond : T -> Bool), ∀ (body : T -> CapsM T), CapsWhileSpec cond body
